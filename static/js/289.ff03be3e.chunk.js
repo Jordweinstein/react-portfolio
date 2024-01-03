5aed2be15e5094fbd04b88c027e6f4a2de7cc22b
Jordweinstein <jordanweinstein556@gmail.com>
2024-01-03 12:43:33 -0500

    Updates

---
+++ static/js/289.ff03be3e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_portfolio=self.webpackChunkreact_portfolio||[]).push([[289],{289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.ff03be3e.chunk.js.map
