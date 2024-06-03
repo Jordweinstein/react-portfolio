ac80d871cc0dc07eef2caab65b94ec14b7199de3
Jordweinstein <jordanweinstein556@gmail.com>
2024-06-03 16:34:02 -0400

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import './styles/Navbar.css';
 
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
+  const location = useLocation();  // Get current location object
+  const toggleMenu = () => setShowMenu(!showMenu);
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+  // Determine if we are on the "About" page
+  const onAboutPage = location.pathname === '/';
 
   return (
-    <>
-      <header>
-        <h1>Jordan Weinstein</h1>
-        <nav className={`navbar ${showMenu ? 'show-menu' : ''}`}>
-          <div className="menu-icon" onClick={toggleMenu}>
-            &#9776;
-          </div>
-          <div className={`nav-links ${showMenu ? 'mobile' : ''}`}>
-            <RouterLink to="/">
-              About
-            </RouterLink>
-            <RouterLink to="/experience">Experience</RouterLink>
+    <header>
+      <h1>Jordan Weinstein</h1>
+      <nav className={`navbar ${showMenu ? 'show-menu' : ''}`}>
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776;
+        </div>
+        <div className={`nav-links ${showMenu ? 'mobile' : ''}`}>
+          <NavLink to="/" className="navbar-item" activeClassName="active">
+            About
+          </NavLink>
+          <NavLink to="/experience" className="navbar-item" activeClassName="active">
+            Experience
+          </NavLink>
+          {onAboutPage ? (
             <ScrollLink
               activeClass="active"
               to="contact"
@@ -30,12 +33,22 @@ export default function Navbar() {
               smooth={true}
               offset={-70}
               duration={500}
+              className="navbar-item"
             >
               Contact
             </ScrollLink>
-          </div>
-        </nav>
-      </header>
-    </>
+          ) : (
+            <NavLink
+              to="/#contact" 
+              className="navbar-item"
+              activeClassName="active"
+            >
+              Contact
+            </NavLink>
+          )}
+        </div>
+      </nav>
+    </header>
   );
 }
+
